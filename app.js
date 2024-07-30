873239077fec0e9d41d63071be2dc17b55579f69
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-07-30 09:18:33 -0400

    update

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-import express from 'express';
 
 
-const app = express()
+
+const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+    response.send("welcome to the API")
+})
