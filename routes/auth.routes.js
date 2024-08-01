2408f14bacf9ab377bcc517dc86515b7d0d023cc
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-08-01 16:33:25 -0400

    update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,14 @@
-import { loginSchema } from "../models/uesrModel.js";
+import { loginSchema, userSchema } from "../models/uesrModel.js";
 import { catchAsync } from "../utils/catchAsync.js";
 import bodyValidator from "../validation/bodyValidator.js";
 import * as userService from '../services/user.service.js'
 import express from 'express';
 const authRoutes = express.Router()
 
+authRoutes.post('/register',
+    bodyValidator(userSchema),
+    catchAsync(userService.createUser)
+)
 authRoutes.post('/login',
     bodyValidator(loginSchema),
     catchAsync(userService.loginUser)
--- services/user.service.js
+++ services/user.service.js
@@ -1,6 +1,50 @@
 import { User } from "../models/uesrModel.js";
 import bcrypt from "bcryptjs";
 import jsonwebtoken from 'jsonwebtoken'
+
+const createUser = async (req, res) => {
+    const { name, email, phoneNumber, password } = req.body;
+    const emailExist = await User.findOne({ email });
+    const passwordExist = await User.findOne({ password });
+
+    if (emailExist || passwordExist) {
+        res.status(201).json({
+            success: false,
+            message: `${emailExist ? "Email" : "Password"} already Taken`,
+        })
+        return;
+    }
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+        name,
+        email,
+        phoneNumber,
+        password: encryptedPassword,
+    })
+    try {
+        const savedUser = newUser.save();
+        const token = jsonwebtoken.sign(
+            {
+                userId: savedUser._id,
+                role: savedUser.role,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "4h" }
+        )
+        res.status(201).json({
+            success: true,
+            message: "user created successfully",
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "server error",
+        })
+    }
+}
+
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
@@ -23,7 +67,7 @@ const loginUser = async (req, res) => {
     if (!validPassword) {
         res.status(400).json({
             success: false,
-            message: "invald credentials"
+            message: "invalid credentials"
         })
     }
     if (validPassword) {
@@ -49,4 +93,7 @@ const loginUser = async (req, res) => {
 
 
 }
-export { loginUser }
+export {
+    loginUser,
+    createUser
+}
